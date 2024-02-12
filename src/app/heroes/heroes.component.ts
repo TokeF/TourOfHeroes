b67a6582ada1c9c42b8a53d9e252b428065aaf4b
TokeF <42637648+TokeF@users.noreply.github.com>
2024-02-12 14:31:52 +0100

    refactored to hero-detail component, moving to part 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { Hero } from './hero';
+import { Hero } from '../hero';
 import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {HEROES} from './mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   standalone: true,
@@ -12,6 +13,7 @@ import {HEROES} from './mock-heroes';
     FormsModule,
     NgFor,
     NgIf,
+    HeroDetailComponent,
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
