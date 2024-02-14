6e6f490bbfea88e7a0e9d0f60effc7d7420b4141
TokeF <42637648+TokeF@users.noreply.github.com>
2024-02-14 17:30:08 +0100

    finished routing module and page navigation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: [ './dashboard.component.css' ],
+  standalone: true,
+  imports: [
+    NgFor,
+    RouterLink
+  ]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { MessageService } from '../message.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -15,6 +16,7 @@ import { MessageService } from '../message.service';
     NgFor,
     NgIf,
     HeroDetailComponent,
+    RouterLink
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
@@ -24,11 +26,6 @@ export class HeroesComponent {
   selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes =   heroes);;
